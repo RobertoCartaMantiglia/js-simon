4971f4bf95e16d6a13cee6d2af6938fa39b8c419
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-02 17:30:41 +0100

    click button

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@
 // rima di dutto dobbiamo costruire una variabile che possa tenere il conto 
 
 let count = 0;
+
+// richiamiamo il bottone dal html
+
+const playButton = document.querySelector("button");
+
+// aggiungiamo l'evento click 
+
+playButton.addEventListener("click", function(){
+    count ++;
+    
+    playButton.innerHTML = `Play (${count})`;
+})
